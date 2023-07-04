755cd205e9b231b588a37d827b9acec4bd03807b
Jaskaran Kamboj <jaskaran.kamboj@jaskaran.kamboj-C02FD4UVML7H>
2023-07-04 16:23:45 +0530

    refactored

--- src/main/java/com/example/demo/Agent.java
+++ src/main/java/com/example/demo/Agent.java
@@ -1,9 +1,10 @@
 package com.example.demo;
 
 import com.sun.tools.attach.VirtualMachine;
-import javassist.ClassPool;
-import javassist.CtClass;
-import javassist.CtMethod;
+import javassist.*;
+import javassist.bytecode.CodeAttribute;
+import javassist.bytecode.LocalVariableAttribute;
+import javassist.bytecode.MethodInfo;
 
 import java.io.ByteArrayInputStream;
 import java.lang.instrument.ClassFileTransformer;
@@ -15,9 +16,14 @@ import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 
+
+
 public class Agent {
     private static Set<String> transformedClasses = new HashSet<>();
+
     public static void agentmain(String agentArgs, Instrumentation inst) {
+
+
 
         inst.addTransformer(new ClassFileTransformer() {
 
@@ -34,20 +40,17 @@ public class Agent {
             }
         }, true);
 
-
-
-
         try {
 
             Class<?>[] classes = inst.getAllLoadedClasses();
             for (Class<?> clazz : classes) {
                 if (clazz.getName().equals("com.example.demo.RestController") && !transformedClasses.contains(clazz.getName()))
                 {
+
                     inst.retransformClasses(Class.forName("com.example.demo.RestController"));
                     transformedClasses.add(clazz.getName());
                 }
             }
-//
         } catch (Exception e) {
             throw new RuntimeException("Failed to retransform class", e);
         }
@@ -59,16 +62,12 @@ public class Agent {
 
             ClassPool cp = ClassPool.getDefault();
 
-//
             CtClass cc = cp.makeClass(new ByteArrayInputStream(classfileBuffer));
 
 
             CtMethod k=cc.getDeclaredMethod("greet");
 
-//            k.insertBefore("System.out.println(java.util.Arrays.toString($args));");
-
-
-
+                parameterValues(k);
 
 
             byte[] bytecode = cc.toBytecode();
@@ -79,20 +78,43 @@ public class Agent {
             throw new RuntimeException("Failed to instrument class", e);
         }
     }
-public static int count=0;
+
+        public  static void parameterValues(CtMethod k) throws NotFoundException,CannotCompileException
+        {
+
+            MethodInfo methodInfo = k.getMethodInfo();
+            CodeAttribute codeAttribute = methodInfo.getCodeAttribute();
+            LocalVariableAttribute attr = (LocalVariableAttribute) codeAttribute.getAttribute(LocalVariableAttribute.tag);
+
+            String[] paramNames = new String[k.getParameterTypes().length];
+
+            int pos = Modifier.isStatic(k.getModifiers()) ? 0 : 1;
+            for (int i = 0; i < paramNames.length; i++) {
+                paramNames[i] = attr.variableName(i + pos);
+            }
+
+            StringBuilder beforeCode = new StringBuilder();
+            beforeCode.append("{ System.out.print(\"Non-breaking breakpoint hit at start of method. Arguments: \"); ");
+            for (int i = 0; i < paramNames.length; i++) {
+                beforeCode.append("System.out.print(\"" + paramNames[i] + "=\" + $args[" + i + "] + \", \"); ");
+            }
+            beforeCode.append("System.out.println(\"\"); }");
+
+            k.insertBefore(beforeCode.toString());
+//            k.insertAfter("System.out.println(\"Calling method: \" + javassist.runtime.Cflow.name($cflow));");
+//            k.insertAfter("{ System.out.println(\"Non-breaking breakpoint hit at end of method\"); }");
+        }
     public static void main(String[] args)  throws Exception{
-        VirtualMachine vm = VirtualMachine.attach("89912");
-        System.out.println(count);
+        long pid=ManagementFactory.getRuntimeMXBean().getPid();
+        System.out.println(pid);
+        VirtualMachine vm = VirtualMachine.attach("84402");
+
         try {
-//			ClassPool cp = ClassPool.getDefault();
-
             vm.loadAgent("/Users/jaskaran.kamboj/Downloads/demo/my-javaagent.jar");
-
         }
         finally {
             vm.detach();
         }
-
 
 
     }
--- src/main/java/com/example/demo/RestController.java
+++ src/main/java/com/example/demo/RestController.java
@@ -4,23 +4,36 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
 //import org.springframework.web.bind.annotation.RestController;
 
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
 
-
-
-    @GetMapping("/greet/{value}")
-    public String greet(@PathVariable String value)
+    @GetMapping("/greet/{value}/{number}")
+    public String greet(@PathVariable String value,@PathVariable String number)
     {
+        if(value.equals("200"))
+            return "help";
+                greet(number,value);
+
+
 
         return "greetPage";
     }
-@GetMapping("help")
-    public String help()
+@GetMapping("home")
+    public String homePage()
     {
         return "homePage";
     }
 
+    public void testingMethod(List<Integer> example,int ex)
+    {
+        return;
+    }
+
 }
+
+
